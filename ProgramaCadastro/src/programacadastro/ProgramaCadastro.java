6923bbb3eb63b390d35f6ff9f059a44eb58595d8
rhuanoliveira <rhuandeoliveiraa@outlook.com>
2015-10-25 20:00:49 -0200

    Início do programa.. Modificações na classe principal, realizando a chamada da Tela Principal

---
+++ ProgramaCadastro/src/programacadastro/ProgramaCadastro.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacadastro;
+
+import Interfaces.TelaPrincipal;
+
+/**
+ *
+ * @author Rhuan
+ */
+public class ProgramaCadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaPrincipal frm = new TelaPrincipal();
+        frm.setVisible(true);
+    }
+    
+}
